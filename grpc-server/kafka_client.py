7c93ce0d33053ba93e412866ba20086bec103a7b
Truong <truongdzz123@gmail.com>
2025-11-27 01:29:42 +0700

    update grpc server

--- grpc-server/kafka_client.py
+++ grpc-server/kafka_client.py
@@ -1,27 +1,6 @@
 import json
 from confluent_kafka import Producer
 
-conf = {
-    'bootstrap.servers': 'worker-01:30092,worker-02:30192,worker-03:30292'
-}
-producer = Producer(conf)
-
-def delivery_report(err, msg):
-    if err is not None:
-        print(f"Delivery failed: {err}")
-    else:
-        print(f"Message delivered to {msg.topic()} [{msg.partition()}]")
-        
-for i in range(10):
-    message = f'Hello Kafka {i}'
-    producer.produce('TEST_TOPIC', message.encode('utf-8'), callback=delivery_report)
-    producer.poll(0) # Trigger delivery callback
-    
-producer.flush() # Wait for all messages to be delivered callback
-
-
-
-# -------------------------------------------------
 
 def read_json_file(filename):
     """Loads a JSON file into a Python dictionary."""
