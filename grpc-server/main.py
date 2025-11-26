58baaf0452c26899df2b70e5ee88ebaefd46cab0
Truong <truongdzz123@gmail.com>
2025-11-27 01:27:57 +0700

    update grpc server

--- grpc-server/main.py
+++ grpc-server/main.py
@@ -5,18 +5,19 @@ from grpc_server import GrpcServer
 from kafka_client import KafkaClient
 
 # -------------------------------------- CONFIGURATION --------------------------------------
-REQUIRED_ARGS_COUNT = 1
+REQUIRED_ARGS_COUNT = 2
 
 if len(sys.argv) != REQUIRED_ARGS_COUNT + 1:
-    print(f"Incorrect argument. Usage: {sys.argv[0]} <GRPC_MAX_WORKERS>")
+    print(f"Incorrect argument. Usage: {sys.argv[0]} <GRPC_MAX_WORKERS> <KAFKA_CONFIG_FILE>")
     
 GRPC_MAX_WORKERS = int(sys.argv[1])
+KAFKA_CONFIG_FILE = str(sys.argv[2])
 CONSUME_TOPIC = "ANALYSIS-COMMAND"
 PRODUCE_TOPIC = "MONITOR-DATA"
 
 def main():
     try:
-        kafka_client = KafkaClient(PRODUCE_TOPIC, CONSUME_TOPIC, 'kafka-config.json')
+        kafka_client = KafkaClient(PRODUCE_TOPIC, CONSUME_TOPIC, KAFKA_CONFIG_FILE)
         grpc_server = GrpcServer(GRPC_MAX_WORKERS, kafka_client)
         grpc_server.startServer()
         
