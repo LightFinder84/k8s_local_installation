fdd139281b10d031c18ed27f058c2d4eff0ceea7
Truong <truongdzz123@gmail.com>
2025-11-27 03:28:14 +0700

    add alalysis app

--- analysis-application/analysis.py
+++ analysis-application/analysis.py
@@ -2,18 +2,22 @@
 class Analysis():
     
     def run(self, data):
+        
         print(f"Running ananlysis with data: {data}")
         if data['metric'] == 'cpu' and float(data['value']) > 50:
+            print("aaaaaaaaaaaaaaaaaaaaaaaaaaaa")
             return {
                 "hostname": data['hostname'],
                 "command": 1,
                 "parameter": 10
             }
         elif data['metric'] == 'cpu' and float(data['value']) <= 50:
+            print("bbbbbbbbbbbbbbbbbbbbbbbb")
             return {
                 "hostname": data['hostname'],
                 "command": 1,
                 "parameter": 5
             }
         else:
+            print("cccccccccccccccccccccccccccc")
             return None
