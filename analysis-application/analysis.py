6d91646177ae34da8681171c185b94ab6b1340dd
Truong <truongdzz123@gmail.com>
2025-11-27 15:48:11 +0700

    update analysis output

--- analysis-application/analysis.py
+++ analysis-application/analysis.py
@@ -2,13 +2,13 @@
 class Analysis():
     
     def run(self, data):
-        if data['metric'] == 'cpu' and float(data['value']) > 50:
+        if data['metric'] == 'cpu' and float(data['value']) > 4:
             return {
                 "hostname": data['hostname'],
                 "command": 1,
                 "parameter": 10
             }
-        elif data['metric'] == 'cpu' and float(data['value']) <= 50:
+        elif data['metric'] == 'cpu' and float(data['value']) <= 4:
             return {
                 "hostname": data['hostname'],
                 "command": 1,
